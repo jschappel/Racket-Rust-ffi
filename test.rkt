65baaf2bd49ad65daed85b8b765b194cff9d904a
Joshua Schappel <jmschappel12@gmail.com>
2020-02-03 12:27:24 -0500

    Added working files

--- test.rkt
+++ test.rkt
@@ -3,3 +3,9 @@
 
 (define rust-lib (ffi-lib
                   (build-path (current-directory) "racketffi" "target" "debug" "racketffi")))
+
+
+(define rust-add (get-ffi-obj "add_from_rust" rust-lib
+                              (_fun _int _int -> _int)))
+
+(rust-add 5 4)
