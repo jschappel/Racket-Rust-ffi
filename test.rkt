d46c83426d73cad33c4906bec3a5565c18c46a9f
Joshua Schappel <jmschappel12@gmail.com>
2020-02-03 12:33:03 -0500

    finished adding files

--- test.rkt
+++ test.rkt
@@ -1,11 +1,14 @@
 #lang racket
 (require ffi/unsafe)
 
+;; add the rust library
 (define rust-lib (ffi-lib
                   (build-path (current-directory) "racketffi" "target" "debug" "racketffi")))
 
 
+;; set the rust add fine to a variable
 (define rust-add (get-ffi-obj "add_from_rust" rust-lib
                               (_fun _int _int -> _int)))
 
+;; call the function
 (rust-add 5 4)
