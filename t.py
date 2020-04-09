2482ccf849584fe653445cc6a1a6cab2c25b0ebb
Joshua Schappel <jmschappel12@gmail.com>
2020-04-09 16:48:49 -0400

    Added necessary files and removed rust project

--- t.py
+++
@@ -1,5 +0,0 @@
-import ctypes
-
-library_name = "./racketffi/target/debug/racketffi.dll"
-rusttools = ctypes.CDLL(library_name)
-print(rusttools.add_from_rust(20, 10))
--- test.rkt
+++ test.rkt
@@ -1,14 +1,26 @@
 #lang racket
 (require ffi/unsafe)
 
-;; add the rust library
-(define rust-lib (ffi-lib
-                  (build-path (current-directory) "racketffi" "target" "debug" "racketffi")))
+;; add the c library
+(define graph-lib (ffi-lib
+                  (build-path (current-directory) "graphviz")))
 
 
 ;; set the rust add fine to a variable
-(define rust-add (get-ffi-obj "add_from_rust" rust-lib
-                              (_fun _int _int -> _int)))
+(define hello (get-ffi-obj "message" graph-lib
+                              (_fun -> _void)))
 
-;; call the function
-(rust-add 5 4)
+
+(define add (get-ffi-obj "add" graph-lib
+                             (_fun _int _int -> _int)))
+                             
+(define add_pointer (get-ffi-obj "add_pointer" graph-lib
+                             (_fun _pointer _pointer -> _void)))
+
+(add 67 33)
+
+
+(define j 10)
+(define k 30)
+(add_pointer j k)
+(println j);
